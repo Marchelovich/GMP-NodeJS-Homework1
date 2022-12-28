55eff136df6372eba4693f9a72fa833ad90af974
OleksandrBabichTR <oleksandr.babich@thomsonreuters.com>
2022-12-28 13:22:35 +0100

    task1: rename params task2: change json structure

--- task1.js
+++ task1.js
@@ -1,14 +1,13 @@
 function reverseString(str) {
     let newString = "";
-    // skip the new line symbol
-    for (let i = str.length - 2; i >= 0; i--) {
+    for (let i = str.length - 1; i >= 0; i--) {
         newString += str[i];
     }
     return newString;
 }
 
 console.log('Enter your text:');
-process.stdin.on('data', data => {
-    let str = data.toString();
+process.stdin.on('data', chunk => {
+    const str = chunk.toString();
     console.log(reverseString(str));
 });
--- task2.js
+++ task2.js
@@ -4,10 +4,21 @@ import csv from "csvtojson";
 const readFilePath = "./upload/test.csv";
 const writeFilePath = "./upload/result.txt";
 
-const readStream = fs.createReadStream(readFilePath)
-    .on("error", (e) => {console.log(e)});
-const writeStream = fs.createWriteStream(writeFilePath)
-    .on("error", (e) => {console.log(e)});
+const readStream = fs.createReadStream(readFilePath);
+const writeStream = fs.createWriteStream(writeFilePath);
+const csvParams = {
+    delimiter: ";",
+    noheader: false,
+    headers: ['book','author', 'amount', 'price']
+};
 
-readStream.pipe(csv()).pipe(writeStream).on("error", (e) => console.log(e));
+readStream.pipe(csv(csvParams).subscribe((json)=>{
+        return new Promise((resolve, reject)=>{
+            delete json['amount'];
+            json.price = Number(json.price);
+            resolve();
+        })})
+    )
+    .pipe(writeStream)
+    .on("error", console.log);
 
